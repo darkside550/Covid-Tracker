3a13edab060f7498554b8be10b1d5021645f8876
darkside550 <imran.saad50@gmail.com>
2020-04-08 11:03:10 +0530

    death and recovery parameter checked

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -30,7 +30,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5" gutterBottom>
-                            <CountUp start={0} end={deaths.value} duration={2.5} separator=","/>
+                            <CountUp start={0} end={recovered.value} duration={2.5} separator=","/>
                         </Typography>
                         <Typography color="textSecondary" gutterBottom>{new Date (lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2" gutterBottom>Number of Recoveries from COVID-19</Typography>
@@ -40,7 +40,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5" gutterBottom>
-                            <CountUp start={0} end={recovered.value} duration={2.5} separator=","/>
+                            <CountUp start={0} end={deaths.value} duration={2.5} separator=","/>
                         </Typography>
                         <Typography color="textSecondary" gutterBottom>{new Date (lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2" gutterBottom>Number of Deaths caused by COVID-19</Typography>
